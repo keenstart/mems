465e2ff08a8ed0472e234fffdf7eb8e4fb60cf96
Gareth harris <memhereroot@gmail.com>
2016-12-20 12:17:00 -0600

    scripts#1

---
+++ includes/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Belal
+ * Date: 12/08/16
+ * Time: 7:58 PM
+ */
+ 
+define('DB_USERNAME', 'mem');
+define('DB_PASSWORD', 'keen');
+define('DB_HOST', '35.162.48.100');
+define('DB_NAME', 'memhere0316');
