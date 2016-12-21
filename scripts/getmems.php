2d04ab22c1c4140b2d65bc3e186d9ae769e26844
Gareth harris <memhereroot@gmail.com>
2016-12-21 12:25:54 -0600

    scripts#10

--- scripts/getmems.php
+++ scripts/getmems.php
@@ -3,7 +3,7 @@
 //creating response array
 $response = array();
  
-if($_SERVER['REQUEST_METHOD']=='POST') {
+if($_SERVER['REQUEST_METHOD']=='GET') {
 
     //including the db operation file
     require_once '../includes/DBOperation.php';
@@ -29,7 +29,7 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 		echo json_encode($resultArray);
 	}	else{
     	$response['error']=true;
-    	$response['message']='You are not authorized';
+    	$response['message']='You are not authorizeds';
     	echo json_encode($response);
 	}    
 
